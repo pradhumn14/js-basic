7d8900540a18822151d93a9771b1018df5aff0dc
Pradhumn <pradhumnagrawal14@gmail.com>
2023-01-30 16:44:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// This is my first Javascript Code!
+// console.log('Hello World');
+
+// let name = 'Pradhumn';
+// console.log(name);
+
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with a number (1name)
+// cannot contain a space or hyphen
+// are case sensitive
+
+// let firstName;
+// let FirstName;
+
+// let firstName = 'Pradhumn';
+// let lastName = 'Agrawal';
+
+// console.log(firstName + " " + lastName);
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+let name = 'Pradhumn'; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+
+let firstName = undefined; // not so common to use undefined
+let selectedColor = null;
